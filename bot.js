a954523a6bd8fc4b16a04a4a6297f32d65d6e084
WallaceWebs <25995763+WallaceWebsBR@users.noreply.github.com>
2018-12-03 02:07:44 -0200

    Fixed Private and Groups Chat

--- bot.js
+++ bot.js
@@ -23,38 +23,9 @@
 		"selected_title": [1, 0, 5, 3, 0, 1, 1, 0, 0, 0, 0]
 	};
 
-	const jokeList = [
-		`
-		Husband and Wife had a Fight.
-		Wife called Mom : He fought with me again,
-		I am coming to you.
-		Mom : No beta, he must pay for his mistake,
-		I am comming to stay with U!`,
-
-		`
-		Husband: Darling, years ago u had a figure like Coke bottle.
-		Wife: Yes darling I still do, only difference is earlier it was 300ml now it's 1.5 ltr.`,
-
-		`
-		God created the earth, 
-		God created the woods, 
-		God created you too, 
-		But then, even God makes mistakes sometimes!`,
-
-		`
-		What is a difference between a Kiss, a Car and a Monkey? 
-		A kiss is so dear, a car is too dear and a monkey is U dear.`
-	]
-
-
 	//
 	// FUNCTIONS
 	//
-
-	// Get random value between a range
-	function rand(high, low = 0) {
-		return Math.floor(Math.random() * (high - low + 1) + low);
-	}
 	
 	function getElement(id, parent){
 		if (!elementConfig[id]){
